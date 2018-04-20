03bb319dc073bc1dcac3508e8644cc85653ff7d9
felicia <felicia@hyperarts.com>
2018-04-20 13:07:03 -0700

    Implement ability to add new rooms.

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,18 +5,37 @@ class RoomList extends Component {
         super(props);
         this.state = {
             rooms: [],
+            newRoomName: '',
         };
         this.roomsRef = this.props.firebase.database().ref('rooms');
+        this.updateRooms = this.updateRooms.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     // UTILITY FNS AND EVENT HANDLERS
     componentDidMount() {
+        this.updateRooms();
+    }
+
+    updateRooms() {
         this.roomsRef.on('child_added', snapshot => {
             const room = snapshot.val();
             room.key = snapshot.key;
-            // WHY DOESN'T PUSH() WORK ON NEXT LINE???
+            // WHY DOESN'T push() WORK ON NEXT LINE???
             this.setState({ rooms: this.state.rooms.concat(room) });
         });
+    }
+
+    handleChange(ev) {
+        // update state (local)
+        this.setState({ newRoomName: ev.target.value });
+    }
+
+    handleSubmit(ev) {
+        ev.preventDefault();
+        // update database (remote)
+        this.roomsRef.push().set({ name: this.state.newRoomName });
     }
 
     render() {
@@ -28,6 +47,17 @@ class RoomList extends Component {
                         <h2 key={index}>{room.name}</h2>
                     ))}
                 </div>
+                <form
+                    className="room-form"
+                    onSubmit={e => this.handleSubmit(e)}
+                >
+                    <input
+                        type="text"
+                        value={this.state.newRoomName}
+                        onChange={e => this.handleChange(e)}
+                    />
+                    <input type="submit" value="Add a new room" />
+                </form>
             </div>
         );
     }
